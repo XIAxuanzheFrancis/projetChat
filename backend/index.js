58b0ddc6878906f5a0c5dc7022ae675f8e191e6b
Xuanzhe XIA <xuanzhe.xia@etu.uca.fr>
2024-01-27 22:20:10 +0100

    create user

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,14 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try{
+    const r = await axios.put('https://api.chatengine.io/users/',
+    {username: username, secret: username, first_name: username},
+    {headers:{"private-key": "95c2c314-c3a4-49a1-a75f-db26f3f01eef"}})
+    return res.status(r.status).json(r.data)
+  }catch(e){
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
